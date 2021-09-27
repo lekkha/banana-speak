121841a6d494b517be8df7b1f156c3f4340ec099
Lekha <lekhasoni001@gmail.com>
2021-09-27 17:23:02 +0530

    taking input and console

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
-console.log("script is working from a different file")
+// reference to the button inside JS
+// query slector is a way of asking the browser the element u need which has a particular id as in by inputting a paticular id 
+//the browser gives the particular element to u 
+
+
+var btnTranslate = document.querySelector("#btn-translate");
+var txtInput = document.querySelector("#txt-input");
+
+console.log("txtInput")
+
+
+function clickHandler(){
+    console.log("clicked!");
+    console.log("input", txtInput.value);
+};
+
+btnTranslate.addEventListener("click", clickHandler)
+
+
+
+// listening to events occuring due to user interaction 
+
+//call back - whem this event happens do this 
+//function clickEventHandler() {
+//   console.log("clicked!")         this entire function is call back 
+
